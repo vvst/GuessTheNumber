5336bd71d5b0668ea7a07b32566e22eee3f5afa6
vvst <wojtek.stats@gmail.com>
2017-10-02 20:25:04 +0200

    Add files via upload

---
+++ GuessTheNumber.py
@@ -0,0 +1,25 @@
+#Guess the Number
+
+import random
+
+print("Witaj! Zgadnij liczbę z przediału <1;100>. Masz 6 prób \n")
+wylosowana = random.randint(1, 100)
+zgadywana = 0
+proba = 1
+
+while proba <= 6 and zgadywana != wylosowana:
+    zgadywana = int(input("Zgaduj liczbę: "))
+    if zgadywana > wylosowana:
+        print("Podałeś za dużą liczbę")
+    else:
+        print("Podałeś za małą liczbę")
+    proba += 1
+
+if zgadywana != wylosowana:
+    print("Niestety, wykorzystałeś już 6 prób i nie odgadłeś liczby")
+    print("Wylosowana liczba to: ", wylosowana)
+else:
+    print("Gratulacje! Udało Ci się, ilość prób: ", proba-1)
+
+
+    
